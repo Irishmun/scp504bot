0453abb79fa0499744765d7fa314763c3ba05949
Irishmun <steefiscool70@gmail.com>
2019-03-07 00:34:27 +0100

    Update SCP-504.py

--- SCP-504.py
+++ SCP-504.py
@@ -33,7 +33,6 @@ async def on_message(message):
        
 
 client.login('TOKEN')
-client.run('TOKEN')
 #Item: One mature SCP-504 tomato
 #Subject: D-504-1
 #Spoken: "insert what came after the joke prefix"
