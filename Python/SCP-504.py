20b22f151b32281731c83ccb13cc42e1939ff630
Steef Weijdeven <413985@student.fontys.nl>
2019-03-12 11:44:26 +0100

    Update SCP-504.py
    added image recognition

--- Python/SCP-504.py
+++ Python/SCP-504.py
@@ -6,34 +6,41 @@ import time
 import random
 
 Client = discord.Client()
-client = commands.Bot(command_prefix = "?")
+bot = commands.Bot(command_prefix="?")
 
-joke_list = ["JOKE,", "MOP,", "GRAP," ]
+joke_list = ["JOKE,", "MOP,", "GRAP,"]
 bypass_list = []
-pain_scale = [" Subject suffered a broken nose.", " Subject rendered unconscious.", " Subject killed."," Subject injured, two teeth dislodged.", " Subject severely injured, right eye destroyed by piece of tomato.", " Chipped tooth and hairline jaw fracture.", " Subject is hospitalized with a massive skull fracture."]
+pain_scale = [" Subject suffered a broken nose.", " Subject rendered unconscious.", " Subject killed.", " Subject injured, two teeth dislodged.",
+              " Subject severely injured, right eye destroyed by piece of tomato.", " Chipped tooth and hairline jaw fracture.", " Subject is hospitalized with a massive skull fracture."]
 
-@client.event
+
+@bot.event
 async def on_ready():
     print("Bot is ready!")
 
 
-@client.event
+@bot.event
 async def on_message(message):
-    x = random.randint(1,1000)
+    if message.author == Client.user:
+        return
+    if message.author.bot:
+        return
+    x = random.randint(1, 767)
     contents = message.content.split(" ")
-    #msg = message.content.split(" ", 1)
+    msg = message.content.split(" ", 1)
+    response = ("```Item: One mature SCP-504 tomato \n" + "Subject: D-504-1 \n" + "Spoken: " +
+                str(msg) + "\n" + "Tomato clocked at " + str(x) + " mph." + random.choice(pain_scale) + "```")
+
     for word in contents:
         if word.upper() in joke_list:
-            if not message.author.id in bypass_list:   
-                 await client.send_message(message.channel,"```Item: One mature SCP-504 tomato \n" + "Subject: D-504-1 \n" +"Spoken: "+ "[REDACTED]" +"\n" + "Tomato clocked at " + str(x)  +" mph." + random.choice(pain_scale) +"```" )
+            if not message.author.id in bypass_list:
+                 await bot.send_message(message.channel, str(response))
+            else:
+                print("shit's in bypass list boi.")
+        else:
+           # print("Image file detected")
+            if message.attachments:
+                await bot.send_message(message.channel, str(response))
 
-  # message.content = message.content.lower()
-    #if message.content in joke_list:
-    #if joke_list in message.content.lower():
-       
-
-client.run('TOKEN')
-#Item: One mature SCP-504 tomato
-#Subject: D-504-1
-#Spoken: "insert what came after the joke prefix"
-#Result: Tomato clocked at "insert random number here" mph. "insert damage here".
+client.run('TOKEN')    
+# "[REDACTED]"
